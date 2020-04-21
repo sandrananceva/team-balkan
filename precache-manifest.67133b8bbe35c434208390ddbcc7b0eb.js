3c34ee6105432bc85dda2f5d785d4b60420e5516
Sandra Nancheva <sandra.nanceva@gmail.com>
2020-04-22 01:38:43 +0200

    Updates

---
+++ precache-manifest.67133b8bbe35c434208390ddbcc7b0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2146cdb039a1f057be0ec76ab1a47c8",
+    "url": "/team-balkan/index.html"
+  },
+  {
+    "revision": "8710446cf25121256c3a",
+    "url": "/team-balkan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75714c5fe8fba34fb141",
+    "url": "/team-balkan/static/js/2.56d36bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team-balkan/static/js/2.56d36bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8710446cf25121256c3a",
+    "url": "/team-balkan/static/js/main.9d02fbfa.chunk.js"
+  },
+  {
+    "revision": "9e2b2d9f69eafa527be6",
+    "url": "/team-balkan/static/js/runtime-main.8ae64d37.js"
+  }
+]);
